b2c7f3812eac791e6f9f7b5aaebf1e9fdfafa01d
Deetya S Salian <121785235+deetyasalian@users.noreply.github.com>
2025-02-26 13:31:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,12 @@ with st.expander('Data'):
 with st.expander('Data visualization'):
   st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g',color='species')
 
+# Data Preparation
+with st.sidebar:
+  st.header('Input features')
+  "","bill_depth_mm","flipper_length_mm","body_mass_g"
+  island=st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+  gender=st.selectbox('Gender',('male','female'))
+  bill_length_mm=st.slider('Bill length(mm)',32.1,59.6,43.9)
+
   
